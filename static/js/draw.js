254383d80e05c57c1e52ef7fd310e6bb368fbf5d
JeyunSong <jyunart@naver.com>
2022-09-21 11:28:52 +0900

    병합중

--- static/js/draw.js
+++ static/js/draw.js
@@ -137,6 +137,12 @@ function send_post(num) {
     window.location.href='/user/'+postnum
 }
 
+ function logout(){
+        $.removeCookie('mytoken');
+        alert('로그아웃!')
+        window.location.href='/login'
+      }
+
 
 
 
